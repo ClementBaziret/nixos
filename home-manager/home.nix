d872ef2208261946bb68d4cef4fa20e22135368f
Clément Baziret <clement.baziret@epitech.eu>
2025-02-11 09:44:22 +0100

    :truck: Moved hyprland config from home.nix to hyprland/hyprland.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,13 +18,14 @@ rec {
       inherit lib;
       inherit vscode-extensions;
     })
-    ./gnome.nix
-    ./bash.nix
-    ./git.nix
     (import ./starship/starship.nix {
       inherit lib;
       inherit (home) username;
     })
+    # ./gnome.nix
+    ./hyprland/hyprland.nix
+    ./bash.nix
+    ./git.nix
   ];
 
   home.sessionVariables = {
@@ -33,10 +34,6 @@ rec {
   };
 
   home.packages = with pkgs; [
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
@@ -46,61 +43,6 @@ rec {
     enable = true;
     enableBashIntegration = true;
     configDir = pkgs.eww;
-    # configDir = ./eww-config-dir;
-    # configDir = /home/cbaziret/.config/eww;
-  };
-
-  # wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.variables = [ "--all" ];
-    extraConfig = ''
-
-    '';
-    settings = {
-      xwayland = {
-        force_zero_scaling = true;
-      };
-
-      input = {
-        kb_layout = "fr";
-        # kb_variant =
-        # kb_model =
-        # kb_options =
-        # kb_rules =
-
-        follow_mouse = 1;
-
-        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
-
-        touchpad = {
-          natural_scroll = true;
-        };
-      };
-      gestures = {
-        workspace_swipe = true;
-      };
-
-      "$terminal" = "kitty";
-      "$fileManager" = "dolphin";
-      "$menu" = "wofi --show drun";
-
-      # See https://wiki.hyprland.org/Configuring/Keywords/
-      "$mainMod" = "SUPER"; # Sets "Windows" key as main modifier
-
-      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      "bind" = [
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, $menu"
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
-      ];
-
-    };
   };
 
   # Home Manager is pretty good at managing dotfiles. The primary
